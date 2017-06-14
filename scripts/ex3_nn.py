5b35ccf67e1345b0c1b49fe0284318ba2c6c7b0d
Ashish Raste <rasteashish@gmail.com>
2017-06-14 08:12:54 +0800

    fix indent spaces

--- scripts/ex3_nn.py
+++ scripts/ex3_nn.py
@@ -27,14 +27,14 @@ def loadData():
 
 
 def loadWeights():
-	"""
-	@brief      Loads pre-trained weights.	
-	"""
-	global theta1, theta2
-	mat = loadmat(weightsFile)
-	theta1 = mat['Theta1']
-	theta2 = mat['Theta2']
-	# print theta1.shape, theta2.shape
+  """
+  @brief      Loads pre-trained weights.	
+  """
+  global theta1, theta2
+  mat = loadmat(weightsFile)
+  theta1 = mat['Theta1']
+  theta2 = mat['Theta2']
+  # print theta1.shape, theta2.shape
 
 
 def sigmoid(Z):
@@ -45,25 +45,25 @@ def sigmoid(Z):
 
 
 def predictAccuracy(theta1, theta2, X):
-	"""
-	@brief      Predicts the accuracy of trained parameters (theta-matrices).
-	
-	@param      theta1  parameters mapping layer 1 to layer 2 of the neural-network.
-	@param      theta2  parameters mapping layer 2 to layer 3 of the neural-network.
-	@param      X       Samples from the dataset, each of them described by their pixel values (400 in number).
-	
-	@return     Prediction of the numbers.
-	"""
-	z2 = theta1.dot(X.T)
-	a2 = sigmoid(z2)
-	a2 = np.r_[np.ones((1,numSamples)),a2]
-	z3 = theta2.dot(a2)
-	a3 = sigmoid(z3)  # shape 10x5000
-	return np.argmax(a3.T,axis=1)+1  # return the predicted values for each of the 5000 samples
+  """
+  @brief      Predicts the accuracy of trained parameters (theta-matrices).
+
+  @param      theta1  parameters mapping layer 1 to layer 2 of the neural-network.
+  @param      theta2  parameters mapping layer 2 to layer 3 of the neural-network.
+  @param      X       Samples from the dataset, each of them described by their pixel values (400 in number).
+
+  @return     Prediction of the numbers.
+  """
+  z2 = theta1.dot(X.T)
+  a2 = sigmoid(z2)
+  a2 = np.r_[np.ones((1,numSamples)),a2]
+  z3 = theta2.dot(a2)
+  a3 = sigmoid(z3)  # shape 10x5000
+  return np.argmax(a3.T,axis=1)+1  # return the predicted values for each of the 5000 samples
 
 
 if __name__=="__main__":
-	loadData()
-	loadWeights()
-	prediction = predictAccuracy(theta1, theta2, X)
-	print 'training accuracy = {}%'.format(np.mean(prediction == Y.ravel())*100)
+  loadData()
+  loadWeights()
+  prediction = predictAccuracy(theta1, theta2, X)
+  print 'training accuracy = {}%'.format(np.mean(prediction == Y.ravel())*100)
